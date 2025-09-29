415622d90abb2a44c7327f4d4fc55360f023f564
praewa-kk <kunsumanufon@gmail.com>
2025-09-29 21:19:10 +0700

    sudoku new version

--- sudoku_web.js
+++ sudoku_web.js
@@ -1,51 +1,80 @@
 //A2 Praewa Boonanan
 //68-010126-1037-5
 
-let a = [
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0]
-];
+let grid = Array.from({ length: 9 }, () => Array(9).fill(0));
+let gridSize = 50;
 
-function setup() {
-  createCanvas(1000,700);
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a[i].length; j++) {
-      let appear = int(random(10));
-      if (appear <= 8) {
-        a[i][j] = int(random(1,10));
+function setup(){
+  createCanvas(1000, 500);
+  createBoard();
+}
+
+function draw(){
+  background(255);
+  drawGrid();
+  drawNum();
+}
+
+function createBoard() {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (int(random(9)) < 5) {
+        grid[row][col] = int(random(1, 10));
+        while (!checkValid(grid, grid[row][col], row, col)){
+          grid[row][col] = int(random(1, 10));
+        }
+      } else {
+        grid[row][col] = 0;
       }
     }
   }
 }
 
-function drawSquare(x,y){
-  line(x*50,y*50,x*50,(y+1)*50);
-  line(x*50,y*50,(x+1)*50,y*50);
-  line((x+1)*50,y*50,(x+1)*50,(y+1)*50);
-  line(x*50,(y+1)*50,(x+1)*50,(y+1)*50);
+function drawGrid(){
+  stroke(0);
+  for (let i = 0; i <= 9; i++) {
+    if (i % 3 === 0) {
+      strokeWeight(3);
+    } else {
+      strokeWeight(1);
+    }
+    line(i * gridSize, 0, i * gridSize, 9 * gridSize);
+    line(0, i * gridSize, 9 * gridSize, i * gridSize);
+  }
 }
 
-function drawGrid(arr){
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      drawSquare(j,i);
-      text(arr[i][j], j*50 + 25, i*50 + 25);
+function drawNum() {
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  fill(0);
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (grid[row][col] !== 0) {
+        text(
+          grid[row][col],col * gridSize + gridSize / 2,row * gridSize + gridSize / 2);
+      }
     }
   }
 }
 
-function draw() {
-  background(200);
-  drawGrid(a);
+function checkValid(arr, num, row, col){
+  let startRow = Math.floor(row / 3) * 3;
+  let startCol = Math.floor(col / 3) * 3;
+  for (let i = startRow; i < startRow + 3; i++) {
+    for (let j = startCol; j < startCol + 3; j++) {
+      if (arr[i][j] === num && !(i === row && j === col)) {
+        return false;
+      }
+    }
+  }
+
+  for (let j = 0; j < 9; j++) {
+    if (j !== col && arr[row][j] === num) return false;
+  }
+
+  for (let i = 0; i < 9; i++) {
+    if (i !== row && arr[i][col] === num) return false;
+  }
+
+  return true;
 }
